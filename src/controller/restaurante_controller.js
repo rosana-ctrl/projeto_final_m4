754c4570c0b0b32528f6aace1f30ae44709a513c
elicadv <102865744+elicadv@users.noreply.github.com>
2022-07-26 20:57:55 -0300

    Adicionei as rotas DELETE/PUT

--- src/controller/restaurante_controller.js
+++ src/controller/restaurante_controller.js
@@ -1,12 +1,24 @@
-import Restaurante from "../model/restaurante_controller.js"
+import Restaurante from "../model/restaurante_model.js"
 
 const hotelRestaurante = (app)=>{
 
 app.get('/restaurante', (req, res)=>{
         
 const restaurante = new Restaurante()
-res.json({"restaurante" : restaurante.garRestaurante})
+res.json({"restaurante" : restaurante.pegarRestaurante()})
     })
+
+app.get('/restaurante/pedido/:pedido', (req, res)=>{
+const restaurante = new Restaurante()
+
+const pedido = req.params.pedido
+
+const pegarPedido = restaurante.pegaUm(pedido)
+
+res.json({"tarefa" : pegarPedido,
+              "erro" : false}
+        )
+})
 
 app.post('/restaurante', (req, res)=>{
 
@@ -18,7 +30,36 @@ restaurante.inserirRestaurante(restaurante)
 
 res.json({"msg" : "Serviço selecionado com sucesso",
           "restaurante" : restaurante,})
-    })
+})
+app.delete('/restaurante/pedido/:pedido', (req,res)=>{
+
+const restaurante = new Restaurante()
+    
+const pedido = req.params.pedido
+    
+restaurante.deletarCardapio(pedido)
+            
+res.json({"msg" : `Pedido ${pedido} deletado com sucesso`,
+              "erro" : false})    
+})
+app.put('/restaurante/pedido/:pedido', (req, res)=>{
+    const body = req.body
+    const pedido = req.params.pedido
+    try {
+        const restaurante = new Restaurante(body.pedido, body.cardapio, body.bebidas,body.cliente)
+        restaurante.atualizarPedido(pedido, restaurante)
+        res.json({
+            "msg" : `Pedido ${pedido} atualizado com sucesso`,
+            "erro" : false
+        })
+
+    } catch (error) {
+        res.json({
+            "msg" : error.message,
+            "erro" : true
+        })
+    }
+})
 }
 
 export default hotelRestaurante
--- src/model/restaurante_model.js
+++ src/model/restaurante_model.js
@@ -14,7 +14,29 @@ class Restaurante {
     pegarRestaurante = ()=>{
         return banco.restaurante
     }
+    pegaUm = (pedido)=>{
+        return banco.restaurante.filter(restaurante=>restaurante.pedido === pedido)
+    }
+
+    deletarCardapio = (pedido)=>{
+        const newDB = banco.restaurante.filter(restaurante=>restaurante.pedido ==!pedido)
+        banco.restaurante = newDB
+    }
+    atualizarPedido = (pedido, novos)=>{
+    const newDb = banco.restaurante.map(restaurante=>{
+    if(restaurante.pedido === pedido){
+    return {
+            "pedido" : novos.pedido || restaurante.pedido,
+            "cardapio" : novos.cardapio || restaurante.cardapio,
+            "bebidas" : novos.bebidas || restaurante.bebidas,
+            "cliente" : novos.cliente || restaurante.cliente
+                }
+            }return restaurante
+        })
+        banco.restaurante = newDb
+    }
 
 }
+
 
 export default Restaurante
