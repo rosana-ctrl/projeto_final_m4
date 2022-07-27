fb9c9a9d4e0e36737ebff2104fa7d78c55a41e11
Sara <saraeliza.lirio@gmail.com>
2022-07-26 22:28:13 -0300

    Atualizando banco de dados, bd.js

--- src/database/bd.js
+++ src/database/bd.js
@@ -1,3 +1,11 @@
+let bd_hospedes = []
+
 let bd_reservas = []
 
-export default bd_reservas
+let bd_servicos = []
+
+let bancoAtividades = []
+
+let restaurante = []
+
+export default {bd_reservas, bd_servicos,bancoAtividades,bd_hospedes,restaurante}
