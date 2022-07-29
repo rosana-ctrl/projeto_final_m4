3d2068ec286a1e88a538dbcdbedbf9751858bd99
Rachelle Santolin <rachelle.santolin@gmail.com>
2022-07-29 19:13:05 -0300

    finalizando create and populate

--- src/database/create-and-populate.js
+++ src/database/create-and-populate.js
@@ -150,6 +150,35 @@ function populaTabelaRestaurante() {
     });
 }
 
+const LAZER_SCHEMA = `
+CREATE TABLE IF NOT EXISTS "LAZER" (
+    "id" INTEGER PRIMARY KEY AUTOINCREMENT,
+    "nome_Hospede" varchar(64),
+    "nome_Atividade" varchar(64),
+    "dia_Atividade" varchar(10) 
+  );`;
+
+const ADD_LAZER_DATA = `
+INSERT INTO LAZER (ID, NOME_HOSPEDE, NOME_ATIVIDADE, DIA_ATIVIDADE)
+VALUES 
+    (1, 'Geraldo Nascimento', 'Trilha', '10/07/2022'),
+    (2, 'Mariah Assunção', 'SPA', '28/07/2022'),
+    (3, 'Raimunda Freitas', 'Yoga', '02/08/2022')
+`
+
+function criaTabelaLzr() {
+    db.run(LAZER_SCHEMA, (error)=> {
+        if (error) console.log("Erro ao criar tabela de lazer");
+    });
+}
+
+
+function populaTabelaLzr() {
+    db.run(ADD_LAZER_DATA, (error)=> {
+        if (error) console.log("Erro ao popular tabela de lazer");
+    });
+}
+
 db.serialize( ()=> {
     criaTabelaReserva();
     populaTabelaReserva();
@@ -159,6 +188,6 @@ db.serialize( ()=> {
     populaTabelaSer();
     criaTabelaRestaurante();
     populaTabelaRestaurante();
-    
-    
+    criaTabelaLzr();
+    populaTabelaLzr();    
 })
