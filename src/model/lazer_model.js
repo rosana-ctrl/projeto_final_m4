623aba77f475e2c883c4cf894e64f66b28a9e766
Edson Vieira <elvieira.275@gmail.com>
2022-08-01 15:39:37 -0300

    alteracao de codigo no arquivo model

--- src/model/lazer_model.js
+++ src/model/lazer_model.js
@@ -1,22 +1,60 @@
-import bd from "../database/bd.js"
-import banco from "../database/bd.js"
+import lazerDAO from "../DAO/lazer_DAO.js"
+import db from "../database/db-sqlite.js"
 
 class Lazer {
-    constructor (faixaEtaria, nomeAtividade, idAtividade) {
-        this.faixaEtaria = faixaEtaria
-        this.nomeAtividade = nomeAtividade
-        this.idAtividade = idAtividade
+   
+    insereAtividade = async(atividade) => {
+        try {
+            const novaAtividade = new validacaoAtividade (atividade.nome_hospede, atividade.nome_atividade, atividade.dia_atividade)
+            lazerDAO.insereAtividade (novaAtividade)
+        } catch (error) {
+            return {
+                "msg": error.message,
+                "erro": true
+            }
+        }
+        
+       
     }
-    escolherAtividades = (bancoAtividades) => {
-        bd.bancoAtividades.push (bancoAtividades)
+    pegaAtividades = async () => {
+        return await lazerDAO.pegarTodosLazer()
     }
 
-    deletaAtividade = (ativ)=>{
-        const newbd = bd.bancoAtividades.filter(atividades => atividades.ativ !== ativ)
-        bd.bancoAtividades = newbd
+    pegaUmaAtividade = async (nome_atividade) => {
+        return await lazerDAO.pegaUmaAtividade (nome_atividade)
     }
+
+    pegaUmNome = async (nome_hospede) => {
+        return await lazerDAO.pegaUmaAtividade (nome_hospede)
+    }
+
+    pegaUmaData = async (dia_atividade) => {
+        return await lazerDAO.pegaUmaAtividade (dia_atividade)
+    }
+
+    deletaAtividade = async (nome_atividade) => {
+        return await lazerDAO.deletaAtividade (nome_atividade)
+    }
+
+    atualizaAtividade = async (nome_atividade, novosDados) => {
+        const atividadeAtualizada = await lazerDAO.atualizaAtividade (novosDados => {
+            if (atividadeAtualizada.nome_atividade === nome_atividade) {
+                return {
+                    "id": atividadeAtualizada.id,
+                    "nome_Hospede": novosDados.nome_hospede || atividadeAtualizada.nome_hospede,
+                    "nome_Atividade": novosDados.nome_atividade || atividadeAtualizada.nome_atividade,
+                    "dia_Atividade": novosDados.dia_atividade || atividadeAtualizada.dia_atividade
+                }
+            }
+            return atividadeAtualizada
+        })
+
+        lazerDAO.atualizaAtividade = atividadeAtualizada
+    }
+
 }
 
-
 export default Lazer
+
+
 
