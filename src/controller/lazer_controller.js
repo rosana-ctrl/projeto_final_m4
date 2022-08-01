3d4e7ba2c852b2f374ff5de653b30d5354fd36b9
Edson Vieira <elvieira.275@gmail.com>
2022-08-01 15:36:59 -0300

    alteracao de codigos no arquivo

--- src/controller/lazer_controller.js
+++ src/controller/lazer_controller.js
@@ -1,41 +1,97 @@
 import Lazer from "../model/lazer_model.js"
-
+import lazerDao from "../DAO/lazer_DAO.js"
+import validacaoAtividade from "../services/validaLazer.js"
+import db from "../database/db-sqlite.js"
 
 const lazer_controller = (app) => {
-    const lazer_model = new Lazer ()
-    app.get ('/lazer', (req, res) => {
-        //const lazer = ("message: Essa chama a model")
-        res.json ('teste lazer')
+    const lazerModel = new Lazer ()    
+    
+    app.get ('/lazer', async(req, res) => {
+        res.json (await lazerDao.pegarTodosLazer(db))
     })
 
-app.post ('/lazer', (req, res) => {
+    app.get('/lazer/atividades/:atividade', async (req, res) => {
+        const atividade = req.params.nome_atividade
+        try {
+            const nome_atividade = await lazerModel.pegaUmaAtividade(atividade)
+            res.json(nome_atividade)
+        } catch (error) {
+            res.json({
+                "msg" : error.message,
+                "erro" : true
+            })
+        }
+    })
+
+    app.get('/lazer/atividades/:hospede', async (req, res) => {
+        const atividade = req.params.nome_hospede
+        try {
+            const nome_hospede = await lazerModel.pegaUmNome(atividade)
+            res.json(nome_hospede)
+        } catch (error) {
+            res.json({
+                "msg" : error.message,
+                "erro" : true
+            })
+        }
+    })
+
+    app.get('/lazer/atividades/:data', async (req, res) => {
+        const atividade = req.params.dia_atividade
+        try {
+            const dia_atividade = await lazerModel.pegaUmaData(atividade)
+            res.json(dia_atividade)
+        } catch (error) {
+            res.json({
+                "msg" : error.message,
+                "erro" : true
+            })
+        }
+    })
+
+app.post ('/lazer', async(req, res) => {
     const body = req.body
     try {
-        const atividades = new Lazer(body.faixaEtaria, body.nomeAtividade, body.idAtividade)
-        atividades.escolherAtividades (atividades)
-        res.json ({"lazer": atividades})
+        const resposta = await lazerModel.insereAtividade(body)
+
+        res.json (resposta)
+
     } catch (error) {
-        res.json(
-            {"msg" : error.message,
-            "erro" : true}
-        ) 
+        res.json({
+            "msg" : error.message,
+            "erro" : true
+        }) 
     }
 })
 
-app.delete ('/lazer/atividades/:atividades', (req,res)=>{
-    const ativ = req.params.ativ
-    lazer_model.deletaAtividade(ativ)
+app.delete ('/lazer/atividades/:atividade', async (req, res) => {
+    const nome_atividade = req.params.nome_atividade
+    try {
+        const resposta = await lazerModel.deletaAtividade(nome_atividade)
+        res.json(resposta)
+    } catch (error) {
+        res.json(error)
+    }
 
-    res.json({
-        "msg" : `Tarefa com titulo ${ativ} deletada com sucesso`,
-        "erro" : false
-    })
 })
+
+app.put ('/lazer/atividades/:atividade', async (req, res) => {
+    const body = req.body
+    const nome_atividade = req.params.nome_atividade
+    try {
+        const novoDados = new validacaoAtividade(body.nome_hospede, body.nome_atividade, body.dia_atividade)
+        const resposta = await lazerModel.atualizaAtividade(nome_atividade, novoDados)
+        res.json(resposta)
+
+    } catch (error) {
+        res.json(error)
+    }
+})
+
 
 }
 
 
 
 export default lazer_controller
-//export default lazer_controller
 
