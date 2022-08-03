cff11365adb8a5e9698e9cf1ac1c90408759d832
elicadv <102865744+elicadv@users.noreply.github.com>
2022-08-03 17:20:24 -0300

    Adicionei o teste unitário

--- src/services/validaRestaurante.js
+++ src/services/validaRestaurante.js
@@ -2,6 +2,8 @@ export const validaCardapio = (cardapio) => {
     if (cardapio) {
         if (cardapio != "strogonoff" && cardapio != "feijoada" && cardapio != "lasanha") {
             throw new Error("Insira um cardapio válido")
+        }else{
+            return cardapio
         }
     } else {
         throw new Error("Cárdapio não foi preenchido")
@@ -13,6 +15,8 @@ export const validaBebida = (bebida) => {
     if (bebida) {
         if (bebida != "suco de laranja" && bebida != "coca-cola" && bebida != "água") {
             throw new Error("Insira uma bebida válida")
+        }else{
+            return bebida
         }
     } else {
         throw new Error("Bebida não foi preenchida")
@@ -21,17 +25,13 @@ export const validaBebida = (bebida) => {
 }
 
 export const validaCliente = (cliente) => {
-    if (cliente) {
-        if (cliente === 0) {
-            throw new Error("Insira um nome")
-        }
-    } else {
+    if (cliente.length == 0) {
         throw new Error("Cliente não foi preenchido")
+    } else {
+        return cliente
     }
 
 }
-
-
 
 export const criaRestaurante = (cardapio, bebida, cliente)=>{
     validaCardapio(cardapio)
