d8c9f37ec67b1674714e7e47581892cb0054d4fb
Rosana Ribeiro <rosana.fribeiro.90@gmail.com>
2022-08-27 00:53:55 -0300

    update database

--- src/services/validacaoReservas.js
+++ src/services/validacaoReservas.js
@@ -1,8 +1,9 @@
 let id_reserva = 21
 
 class ValidacaoReserva {
-    constructor(quarto, quantLeitos, quantAdultos, quantCrian, dataEntrada, dataSaida) {
+    constructor(idhospede, quarto, quantLeitos, quantAdultos, quantCrian, dataEntrada, dataSaida) {
         this.id_reserva = id_reserva++
+        this.idhospede = idhospede
         this.quarto = quarto
         this.quantLeitos = quantLeitos
         this.quantAdultos = quantAdultos
