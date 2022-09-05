3712d7824993783e847935afbe7f78129aa51e38
Rosana Ribeiro <rosana.fribeiro.90@gmail.com>
2022-09-05 17:09:03 -0300

    update reservas

--- src/DAO/reservasDAO.js
+++ src/DAO/reservasDAO.js
@@ -87,7 +87,7 @@ const reservasDAO = {
             db.run(`UPDATE RESERVAS 
             SET ID_HOSPEDE = ?, QUARTO = ?, QUANTLEITOS = ?, QUANTADULTOS = ?, QUANTCRIAN =?, DATAENTRADA = ?, DATASAIDA =?
             WHERE ID = ?`,
-                novosDados.id_Hospede, novosDados.quarto, novosDados.quantLeitos, novosDados.quantAdultos, novosDados.quantCrian, novosDados.dataEntrada, novosDados.dataSaida,
+                novosDados.idhospede, novosDados.quarto, novosDados.quantLeitos, novosDados.quantAdultos, novosDados.quantCrian, novosDados.dataEntrada, novosDados.dataSaida,
                 id,
                 (error) => {
                     if (error) {
